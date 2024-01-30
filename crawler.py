396d8325ad4345415e3bf1340a3f50edb437baaf
Sean Parsonson <sean@midnight.health>
2024-01-30 14:40:39 +1000

    5div

--- crawler.py
+++ crawler.py
@@ -13,7 +13,7 @@ def get_stocks(csv_file):
 
 
 def main():
-	print("stock	pe	mc	div	beta	debttoequity	profitmargin	cashpershare	earningsgrowth	revenuegrowth")
+	print("stock	pe	mc	div5	beta	debttoequity	profitmargin	cashpershare	earningsgrowth	revenuegrowth")
 
 	stocks = get_stocks("asx.csv")
 	stocks.extend(get_stocks("sp500.csv"))
@@ -34,10 +34,11 @@ def main():
 		cash_per_share = round(info["totalCashPerShare"], 2) if "totalCashPerShare" in keys else 0
 		earnings_growth = info["earningsGrowth"] if "earningsGrowth" in keys else 0
 		revenue_growth = info["revenueGrowth"] if "revenueGrowth" in keys else 0
-		div_unconverted = info.get("trailingAnnualDividendYield")
-		div = round(div_unconverted * 100, 2) if div_unconverted is not None else 0
+		# div_unconverted = info.get("trailingAnnualDividendYield")
+		div_five_year = info["fiveYearAvgDividendYield"] if "fiveYearAvgDividendYield" in keys else 0
+		# div = round(div_unconverted * 100, 2) if div_unconverted is not None else 0
 		if pe > 0 and pe < 25 and market_cap > 1:
-			print(f"{stock}	{pe}	{market_cap}	{div}	{beta}	{debt_to_equity}		{profit_margin}		{cash_per_share}		{earnings_growth}		{revenue_growth}")
+			print(f"{stock}	{pe}	{market_cap}	{div_five_year}	{beta}	{debt_to_equity}		{profit_margin}		{cash_per_share}		{earnings_growth}		{revenue_growth}")
 
 if __name__ == "__main__":
     main()
