1394fde4ca3e52aef84a0dece6a4ce1906e2fa0e
Sean Parsonson <sean@midnight.health>
2024-02-08 16:34:24 +1000

    removed pe

--- simulator.py
+++ simulator.py
@@ -13,16 +13,13 @@ def roll():
 def main(args):
     mu = float(args[2])
     sigma = float(args[3])
-    eps = float(args[4])
     twosigma = sigma * 2
     threesigma = sigma * 3
-    average_eps_growth = 0
     zero_count = 0
-    # 1000 universes
-    for x in range(1000):
+    # 100 universes
+    for x in range(100):
         price = float(args[1])
         quantity = round(10000 / price)
-        pe1 = round(price / eps, 2)
         print("")
         print(f"quantity: {quantity} value: {round(price * quantity)}")
         # simulate 10 years of prices
@@ -49,28 +46,22 @@ def main(args):
             # cant have negative prices
             if price < 0 or price < 0.0:
                 price = 0
-                pe = price / eps
-                print(f"{price}     {pe}")
+                print(f"{price}")
                 if x == 9:
-                    eps_growth = round((price/pe1)/eps, 2)
                     value = round(price * quantity)
                     if value == 0:
                         zero_count = zero_count + 1
-                    print(f"quantity: {quantity} value: {value} eps_growth: {eps_growth}")
-                    average_eps_growth = average_eps_growth + eps_growth
+                    print(f"quantity: {quantity} value: {value}")
                 continue
-            pe = round(price / eps, 2)
-            print(f"{round(price, 2)}       {pe}")
+            print(f"{round(price, 2)}")
             if x == 9:
-                eps_growth = round((price/pe1)/eps, 2)
                 value = round(price * quantity)
                 if value == 0:
                     zero_count = zero_count + 1
-                print(f"quantity: {quantity} value: {value} eps_growth: {eps_growth}")
-                average_eps_growth = average_eps_growth + eps_growth
+                print(f"quantity: {quantity} value: {value}")
         # time.sleep(1)
     print("")
-    print(f"average_eps_growth: {round(average_eps_growth/1000, 2)} zero_count: {zero_count}")
+    print(f"zero_count: {zero_count}")
 
 if __name__ == "__main__":
     main(sys.argv)
