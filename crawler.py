cea748d3ff4137eb449561fb3793eda49500cb50
Sean Parsonson <sean@midnight.health>
2024-02-26 16:08:20 +1000

    updates

--- crawler.py
+++ crawler.py
@@ -34,19 +34,23 @@ def main(args):
 	stocks.pop(0)
 	if len(args) == 0:
 		print("running in filter mode")
-		stocks = get_stocks("asx.csv")
+		stocks = get_stocks("sp500.csv")
 		# stocks.extend(get_stocks("sp500.csv"))
-		stocks.extend(get_stocks("nikkei.csv"))
+		# stocks.extend(get_stocks("asx.csv"))
 		# stocks.extend(get_stocks("ftse100.csv"))
 	n = len(stocks)
-	print(f"	pe	mc	div	div5	beta	debttoequity	profitmargin	cashpershare	earningsgrowth	revenuegrowth")
+	print(f"	mc	pe	pb	div	div5	beta	debttoequity	profitmargin	cashpershare	earningsgrowth	revenuegrowth	name")
 	tickers = yf.Tickers(stocks)
 	all_pe = []
 	all_div_five_year = []
 	all_beta = []
+	all_pb = []
 	for stock in tickers.tickers:
 		info = tickers.tickers[stock].info
 		keys = info.keys()
+		name = info["longName"] if "longName" in keys else "None"
+		pb = round(info["priceToBook"], 2) if "priceToBook" in keys and type(info["priceToBook"]) is not str  else 0
+		all_pb.append(pb)
 		currency = info["currency"] if "currency" in keys else 0
 		price = round(convert_currency(info["currentPrice"], currency), 2) if "currentPrice" in keys else 0
 		eps = info["trailingEps"] if "trailingEps" in keys else 0
@@ -70,9 +74,10 @@ def main(args):
 			all_div_five_year.append(div_five_year)
 		else:
 			all_div_five_year.append(div)
-		# if pe > 0 and pe < 25 and market_cap > 1:
-		print(f"{stock}	{pe}	{market_cap}	{div}	{div_five_year}	{beta}	{debt_to_equity}		{profit_margin}		{cash_per_share}		{earnings_growth}		{revenue_growth}")
+		# if pe > 0 and pe < 30 and profit_margin >= 0.10:
+		print(f"{stock}	{market_cap}	{pe}	{pb}	{div}	{div_five_year}	{beta}	{debt_to_equity}		{profit_margin}		{cash_per_share}		{earnings_growth}		{revenue_growth}	{name}")
 	print(f"pe: {round(sum(all_pe)/n, 2)}")
+	print(f"pb: {round(sum(all_pb)/n, 2)}")
 	print(f"div5: {round(sum(all_div_five_year)/n, 2)}")
 	print(f"beta: {round(sum(all_beta)/n, 2)}")
 	print(f"stocks: {n}")
